bab5e1f68aa17a339fc063887e5834b263cfa6f9
SContent44 <23169220+SContent44@users.noreply.github.com>
2020-06-28 23:24:04 +1000

    chore: Update binding example with a variable to hold the input reference to make it easier to share with nested block scopes

--- packages/durandal-starterkit/src/viewmodels/binding/binding.js
+++ packages/durandal-starterkit/src/viewmodels/binding/binding.js
@@ -6,6 +6,16 @@ import dynamicTemplate from "./dynamic.html";
 function BindingViewModel() {
     this.view = viewTemplate;
 
+    // Input for each of the someData values
+    this.sharedInput = ko.observable("Shares parent context").extend({
+        required: true,
+    });
+
+    const ownInputHelper = ko.observable("Own binding context").extend({
+        required: true,
+    });
+    this.ownInput = ownInputHelper;
+
     // Composing a view that does not have it's own viewmodel by just passing view in directly.
     this.sharedVMScreen = dynamicTemplate;
 
@@ -27,15 +37,6 @@ function BindingViewModel() {
         return obj;
     };
 
-    // Input for each of the someData values
-    this.sharedInput = ko.observable("Shares parent context").extend({
-        required: true,
-    });
-
-    this.ownInput = ko.observable("Own binding context").extend({
-        required: true,
-    });
-
     this.anotherOwnVMScreenObject = {
         view: dynamicTemplate,
         model: {
@@ -48,7 +49,7 @@ function BindingViewModel() {
 
     this.anotherOwnVMScreenFunction = function () {
         this.someData = ko.pureComputed(() => {
-            return "Can use a function with its own this context";
+            return ownInputHelper();
         }, this);
         this.viewName = "Another own VM screen";
         this.view = dynamicTemplate;
