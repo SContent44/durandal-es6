14ea7352b7fdc22fec20fdaa87f0f65376f4a665
SContent44 <23169220+SContent44@users.noreply.github.com>
2020-06-28 21:44:16 +1000

    fix: some updates for material dialog and set the button height as the inherit path is too narrow

--- packages/durandal-playground/src/Components/material-button/index.js
+++ packages/durandal-playground/src/Components/material-button/index.js
@@ -6,7 +6,7 @@ const ButtonComponent = {
     viewModel: {
         createViewModel(params, componentInfo) {
             const ButtonViewModel = function () {
-                this.buttonRipple = new MDCRipple(componentInfo.element.querySelector(".mdc-button"));
+                const buttonRipple = new MDCRipple(componentInfo.element.querySelector(".mdc-button"));
                 this.styles = styles;
 
                 this.text = params.text || "Button";
