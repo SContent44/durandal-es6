1da3bb345b676217b6160642241a0f95d46a50ca
SContent44 <23169220+SContent44@users.noreply.github.com>
2020-08-04 03:39:05 +1000

    feat: remove the system.defer (which is just a wrapper for $.deferred) and replace with promises

--- packages/durandal-es6/transitions/transitionHelper.js
+++ packages/durandal-es6/transitions/transitionHelper.js
@@ -64,7 +64,7 @@ const animationTypes = [
 ];
 
 const App = {
-    duration: 1000 * 0.3, // seconds - set to 0 if user has accessibility option for reduced animation
+    duration: 1000 * 0.3,
     create(receievedSettings) {
         const settings = ensureSettings(receievedSettings);
         return doTrans(settings);
@@ -91,45 +91,46 @@ function doTrans(settings) {
     let $previousView;
     let $newView = $(newChild).removeClass(outAn); // just need to remove outAn here, keeping the animated class so we don't get a "flash"
 
-    return system
-        .defer(function (dfd) {
-            if (newChild) {
-                $newView = $(newChild);
-                if (settings.activeView) {
-                    outTransition(inTransition);
-                } else {
-                    inTransition();
+    if (newChild) {
+        $newView = $(newChild);
+        if (settings.activeView) {
+            return outTransition(inTransition);
+        }
+
+        return inTransition();
+    }
+
+    function outTransition(callback) {
+        $previousView = $(activeView);
+        $previousView.addClass("animated");
+        $previousView.addClass(outAn);
+
+        return new Promise(function (resolve) {
+            setTimeout(() => {
+                if (callback) {
+                    resolve(callback());
                 }
-            }
+            }, App.duration);
+        });
+    }
 
-            function outTransition(callback) {
-                $previousView = $(activeView);
-                $previousView.addClass("animated");
-                $previousView.addClass(outAn);
-                setTimeout(function setTimeout() {
-                    if (callback) {
-                        callback();
-                    }
-                }, App.duration);
-            }
+    function inTransition() {
+        if ($previousView) {
+            $previousView.css("display", "none");
+        }
+        settings.triggerAttach();
 
-            function inTransition() {
-                if ($previousView) {
-                    $previousView.css("display", "none");
-                }
-                settings.triggerAttach();
+        $newView.addClass("animated"); // moved the adding of the animated class here so it keeps it together
+        $newView.addClass(inAn);
+        $newView.css("display", "");
 
-                $newView.addClass("animated"); // moved the adding of the animated class here so it keeps it together
-                $newView.addClass(inAn);
-                $newView.css("display", "");
-
-                setTimeout(function setTimeout() {
-                    $newView.removeClass(`${inAn} animated`); // just need to remove inAn here, that's all we'll have
-                    dfd.resolve(true);
-                }, App.duration);
-            }
-        })
-        .promise();
+        return new Promise(function (resolve) {
+            setTimeout(() => {
+                $newView.removeClass(`${inAn} animated`); // just need to remove inAn here, that's all we'll have
+                resolve(true);
+            }, App.duration);
+        });
+    }
 }
 
 export default App;
